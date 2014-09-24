d73918763cda2963028092ba7a4590f4a46d3de2
cuchi <paulo.cuchi@gmail.com>
2014-09-24 16:19:27 -0300

    usage example

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 #include "list.h"
 
 void print_integer(void* n) {
@@ -10,17 +11,33 @@ void add_one(void* n) {
 }
 
 int main() {
+    srand(time(NULL));
+
     // Creating our new list of integers:
     list* l = list_new(sizeof(int), NULL, NULL);
+
     // Let's add some stuff in it.
-    int i, j;
-    for (i = 1; i <= 10; i++) {
-        j = i * i;
-        list_r_append(l, &j);
+    int i, j, k;
+    for (i = 1; i <= 20; i++) {
+        j = rand() % i;
+        k = rand() % 50;
+        printf("Inserting %d in [%d].\n", k, j);
+        list_insert(l, &k, j);
     }
+
     // We can call any function to manipulate
-    // and show the data:
+    printf("Adding one to every element...\n");
     list_foreach(l, add_one);
+
+    // and show the data:
     list_foreach(l, print_integer);
+
+    for (i = 20; i >= 1; i--) {
+        j = rand() % i;
+        int* p = list_remove(l, j);
+        memcpy(&k, p, sizeof(int));
+        free(p);
+        printf("Removed %d in [%d]\n", k, j);
+    }
     return 0;
 }
