3e5d1da8d2ad0f7cbd6096b7646fec8e48f5e08e
cuchi <paulo.cuchi@gmail.com>
2014-09-25 00:34:26 -0300

    usage example

--- main.c
+++ main.c
@@ -10,6 +10,10 @@ void add_one(void* n) {
     (*(int*) n)++;
 }
 
+int cmp_int(void* a, void* b) {
+    return (*(int*) a) - (*(int*) b);
+}
+
 int main() {
     srand(time(NULL));
 
@@ -18,26 +22,16 @@ int main() {
 
     // Let's add some stuff in it.
     int i, j, k;
-    for (i = 1; i <= 20; i++) {
+    // Playing with randomness:
+    for (i = 1; i <= 50; i++) {
         j = rand() % i;
-        k = rand() % 50;
+        k = rand() % 500;
         printf("Inserting %d in [%d].\n", k, j);
         list_insert(l, &k, j);
     }
 
-    // We can call any function to manipulate
-    printf("Adding one to every element...\n");
-    list_foreach(l, add_one);
-
-    // and show the data:
+    printf("Playing with the quicksort:\n");
+    list_quicksort(l, cmp_int);
     list_foreach(l, print_integer);
-
-    for (i = 20; i >= 1; i--) {
-        j = rand() % i;
-        int* p = list_remove(l, j);
-        memcpy(&k, p, sizeof(int));
-        free(p);
-        printf("Removed %d in [%d]\n", k, j);
-    }
     return 0;
 }
